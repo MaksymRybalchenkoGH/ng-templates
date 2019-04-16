52bda82887f0a3cd0ec3ccb6d11be1248f8da68d
Max Rybalchenko <maksimry@rabota.ua>
2019-04-16 19:04:53 +0300

    component for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
+import { AppComponent } from './app.component'
 
 const routes: Routes = [
   {
     path: '',
-    component: 'app',
+    component: AppComponent,
     children: []
   }
 ]
